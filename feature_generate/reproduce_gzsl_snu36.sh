803f1246ed1ed63be0532fce41ea16815e07a674
Seungjun Lee <39257402+7tl7qns7ch@users.noreply.github.com>
2024-01-08 19:11:56 +0900

    Add files via upload

---
+++ feature_generate/reproduce_gzsl_snu36.sh
@@ -0,0 +1,31 @@
+python clswgan.py --syn_num 128 --nz 13 --ngh 1024 --ndh 1024 --nepoch 1000
+
+python clswgan.py --syn_num 256 --nz 13 --ngh 1024 --ndh 1024 --nepoch 1000
+
+python clswgan.py --syn_num 512 --nz 13 --ngh 1024 --ndh 1024 --nepoch 1000
+
+python clswgan.py --syn_num 1024 --nz 13 --ngh 1024 --ndh 1024 --nepoch 1000
+
+python clswgan.py --syn_num 128 --nz 13 --ngh 512 --ndh 512 --nepoch 1000
+
+python clswgan.py --syn_num 256 --nz 13 --ngh 512 --ndh 512 --nepoch 1000
+
+python clswgan.py --syn_num 512 --nz 13 --ngh 512 --ndh 512 --nepoch 1000
+
+python clswgan.py --syn_num 1024 --nz 13 --ngh 512 --ndh 512 --nepoch 1000
+
+python clswgan.py --syn_num 128 --nz 13 --ngh 256 --ndh 256 --nepoch 1000
+
+python clswgan.py --syn_num 256 --nz 13 --ngh 256 --ndh 256 --nepoch 1000
+
+python clswgan.py --syn_num 512 --nz 13 --ngh 256 --ndh 256 --nepoch 1000
+
+python clswgan.py --syn_num 1024 --nz 13 --ngh 256 --ndh 256 --nepoch 1000
+
+
+python clswgan.py --attSize 7 --G_mode split --D_mode split --resultsroot result_4 --dataset SNU36_4sp
+
+python clswgan.py --attSize 7 --G_mode split --D_mode split --resultsroot result_4 --dataset SNU36_4ca
+
+python clswgan.py --attSize 21 --G_mode split --D_mode split --resultsroot result_4 --dataset SNU36_4oh
+
